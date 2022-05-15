79b9eb7cb9db99a9b38ac2a92327903c7d7e392b
재윤 <55301320+jy-km@users.noreply.github.com>
2022-05-15 17:54:14 +0000

    drew pizza

--- main.py
+++ main.py
@@ -5,5 +5,18 @@ pygame.init()
 width = 500
 height = 500
 screen = pygame.display.set_mode([width, height])
+running = True
 
+pizza = pygame.image.load('pizza.png')
+pizza = pygame.transform.scale(pizza, (200,200))
 
+while running == True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+  screen.fill((255,255,255))
+  screen.blit(pizza,(150,150))
+  pygame.display.flip()
+
+pygame.quit()
